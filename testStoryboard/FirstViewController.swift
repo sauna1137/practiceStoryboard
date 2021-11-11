a6291b59027d50f6dc6df19a7aa70fc296582b9b
sauna <soneko2102@gmail.com>
2021-11-11 15:03:48 +0900

    Initial Commit

---
+++ testStoryboard/FirstViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FirstViewController.swift
+//  testStoryboard
+//
+//  Created by KS on 2021/11/11.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
